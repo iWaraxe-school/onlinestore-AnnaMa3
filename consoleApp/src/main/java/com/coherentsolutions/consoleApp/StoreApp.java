5f7901320f2c428a23dd28ffec2040f9067681e9
AnnaMa3 <ann.matveyenko@gmail.com>
2022-11-23 14:26:21 +0100

    Commit 7 - XML reader and parser

--- consoleApp/src/main/java/com/coherentsolutions/consoleApp/StoreApp.java
+++ consoleApp/src/main/java/com/coherentsolutions/consoleApp/StoreApp.java
@@ -1,13 +1,26 @@
 package com.coherentsolutions.consoleApp;
+
 import com.coherentsolutions.store.Store;
 import com.coherentsolutions.store.StoreHelper.RandomStorePopulator;
+import com.coherentsolutions.store.StoreHelper.XMLParser.XmlParser;
+
+import javax.xml.stream.XMLStreamException;
+import java.io.FileNotFoundException;
+import java.util.Map;
 
 public class StoreApp {
-    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+    public static void main(String[] args) throws XMLStreamException, FileNotFoundException {
         Store onlineStore = new Store();
         RandomStorePopulator randomStorePopulator = new RandomStorePopulator(onlineStore);
         randomStorePopulator.fillStore();
         onlineStore.printAll();
+
+
+        Map<String,String> map = XmlParser.parseToMap();
+        for(Map.Entry mp: map.entrySet()){
+            System.out.println("KEY: " + mp.getKey() + ", VALUE: " + mp.getValue());
+        }
+
 
     }
 }
